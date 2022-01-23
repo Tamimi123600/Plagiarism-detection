3b882607bab6897b8a5f37e76b1c5719970405bd
Tamimi123600 <67618337+Tamimi123600@users.noreply.github.com>
2022-01-23 22:41:31 +0300

    Update sklearn(train).py

--- sklearn(train).py
+++ sklearn(train).py
@@ -6,7 +6,6 @@ import pandas as pd
 
 from sklearn.externals import joblib
 
-## TODO: Import any additional libraries you need to define a model
 from sklearn.svm import LinearSVC
 
 
@@ -24,7 +23,6 @@ def model_fn(model_dir):
     return model
 
 
-## TODO: Complete the main code
 if __name__ == '__main__':
     # All of the model parameters and training parameters are sent as arguments
     # when this script is executed, during a training job
@@ -32,13 +30,12 @@ if __name__ == '__main__':
     # Here we set up an argument parser to easily access the parameters
     parser = argparse.ArgumentParser()
 
-    # SageMaker parameters, like the directories for training data and saving models; set automatically
-    # Do not need to change
+   
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
 
-    ## TODO: Add any additional arguments that you will need to pass into your model
+ 
 
     # args holds all passed-in arguments
     args = parser.parse_args()
@@ -51,15 +48,15 @@ if __name__ == '__main__':
     train_y = train_data.iloc[:, 0]
     train_x = train_data.iloc[:, 1:]
 
-    ## --- Your code here --- ##
+  
 
-    ## TODO: Define a model
+    ## Define a model
     model = LinearSVC()
 
-    ## TODO: Train the model
+    ## Train the model
     model.fit(train_x, train_y)
 
-    ## --- End of your code  --- ##
+ 
 
     # Save the trained model
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
